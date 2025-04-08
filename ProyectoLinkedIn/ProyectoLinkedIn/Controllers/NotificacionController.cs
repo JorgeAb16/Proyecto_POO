5d6dbf1f9edb0849f5d2f0253dbc986114a4efde
Richard <richardorlando92@gmail.com>
2025-04-07 21:58:12 -0600

    Probando

---
+++ ProyectoLinkedIn/ProyectoLinkedIn/Controllers/NotificacionController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProyectoLinkedIn.Models;
+
+namespace ProyectoLinkedIn.Controllers
+{
+
+    public class NotificacionController : ApiController
+    {
+        private DBContextProject db = new DBContextProject();
+        // GET: api/Notificacion
+        public IEnumerable<Notificacion> Get()
+        {
+            return db.Notificacion;
+        }
+
+        // GET: api/Notificacion/5
+        public IHttpActionResult Get(int id)
+        {
+            Notificacion notificacion = db.Notificacion.Find(id);
+            if (notificacion == null)
+            {
+                return NotFound();
+            }
+            return Ok(notificacion);
+        }
+
+        // POST: api/Notificacion
+        public IHttpActionResult Post(Notificacion notificacion)
+        {
+            db.Notificacion.Add(notificacion);
+            db.SaveChanges();
+            return Ok(notificacion);
+        }
+
+        // PUT: api/Notificacion/5
+        public IHttpActionResult Put(int id, Notificacion notificacionModificada)
+        {
+            db.Entry(notificacionModificada).State = EntityState.Modified;
+            db.SaveChanges();
+            return Ok(notificacionModificada);
+        }
+
+        // DELETE: api/Notificacion/5
+        public IHttpActionResult Delete(int id)
+        {
+            Notificacion notificacion = db.Notificacion.Find(id);
+            db.Notificacion.Remove(notificacion);
+            db.SaveChanges();
+            return Ok(notificacion);
+        }
+    }
+}
---
+++ ProyectoLinkedIn/ProyectoLinkedIn/Controllers/VacanteController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProyectoLinkedIn.Models;
+
+namespace ProyectoLinkedIn.Controllers
+{
+    public class VacanteController : ApiController
+    {
+        private DBContextProject db = new DBContextProject();
+        // GET: api/Vacante
+        public IHttpActionResult Get()
+        {
+            var vacantes = from vacante in db.Vacante
+                              join empresa in db.Empresa on vacante.EmpresaId equals empresa.Id
+                              select new
+                              {
+                                  ComentarioId = comentario.Id,
+
+                                  IdUsuario = usuario.Id,
+                                  UsuarioNombre = usuario.Nombre,
+
+                                  publicacionId = publicacion.Id,
+                                  publicacionTitulo = publicacion.Titulo,
+
+                                  Contenido = comentario.Contenido,
+                                  FechaPublicacion = comentario.Fechapublicacion,
+                              };
+
+            return Ok(comentarios);
+        }
+
+        // GET: api/Vacante/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Vacante
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Vacante/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Vacante/5
+        public void Delete(int id)
+        {
+        }
+    }
+}

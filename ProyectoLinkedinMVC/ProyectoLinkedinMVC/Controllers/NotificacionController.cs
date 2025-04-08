1f785b9f8cf7dbf9c88d09f546b4de51a303ac6d
Richard <richardorlando92@gmail.com>
2025-04-07 21:54:36 -0600

    Ignorando otro web.config

---
+++ ProyectoLinkedinMVC/ProyectoLinkedinMVC/Controllers/NotificacionController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web.Http;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Newtonsoft.Json;
+using ProyectoLinkedinMVC.Models;
+
+namespace ProyectoLinkedinMVC.Controllers
+{
+    public class NotificacionController : ApiController
+    {
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get(DataSourceLoadOptions loadOptions)
+        {
+            var apiUrl = "https://localhost:44345/api/Notificacion";
+
+            var respuestaJson = await GetAsync(apiUrl);
+            //System.Diagnostics.Debug.WriteLine(respuestaJson); imprimir info
+            List<Notificacion> listaNotificacion = JsonConvert.DeserializeObject<List<Notificacion>>(respuestaJson);
+            return Request.CreateResponse(DataSourceLoader.Load(listaNotificacion, loadOptions));
+        }
+
+        public static async Task<string> GetAsync(string uri)
+        {
+            try
+            {
+                var handler = new HttpClientHandler();
+                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+                using (var client = new HttpClient(handler))
+                {
+                    var response = await client.GetAsync(uri);
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+            }
+            catch (Exception e)
+            {
+                var m = e.Message;
+                return null;
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post(FormDataCollection form)
+        {
+
+            var values = form.Get("values");
+
+            var httpContent = new StringContent(values, System.Text.Encoding.UTF8, "application/json");
+
+            var url = "https://localhost:44345/api/Notificacion";
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (var client = new HttpClient(handler))
+            {
+                var response = await client.PostAsync(url, httpContent);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+
+        [HttpPut]
+        public async Task<HttpResponseMessage> Put(FormDataCollection form)
+        {
+            //Parámetros del form
+            var key = Convert.ToInt32(form.Get("key")); //llave que estoy modificando
+            var values = form.Get("values"); //Los valores que yo modifiqué en formato JSON
+
+            var apiUrlGetNotificacion = "https://localhost:44345/api/Notificacion" + key;
+            var respuestaNotificacion = await GetAsync(apiUrlGetNotificacion = "https://localhost:44345/api/Notificacion" + key);
+            Notificacion notificacion = JsonConvert.DeserializeObject<Notificacion>(respuestaNotificacion);
+
+            JsonConvert.PopulateObject(values, notificacion);
+
+            string jsonString = JsonConvert.SerializeObject(notificacion);
+            var httpContent = new StringContent(jsonString, System.Text.Encoding.UTF8, "application/json");
+
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (var client = new HttpClient(handler))
+            {
+                var url = "https://localhost:44345/api/Notificacion" + key;
+                var response = await client.PutAsync(url, httpContent);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+            }
+
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+
+
+        [HttpDelete]
+        public async Task<HttpResponseMessage> Delete(FormDataCollection form)
+        {
+            var key = Convert.ToInt32(form.Get("key"));
+
+            var apiUrlDelNotificacion = "https://localhost:44345/api/Notificacion" + key;
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (var client = new HttpClient(handler))
+            {
+                var respuestaNotificacion = await client.DeleteAsync(apiUrlDelNotificacion);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+
+    }
+}
